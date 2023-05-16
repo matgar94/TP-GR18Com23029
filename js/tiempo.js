23a55e098de9615e1dd1d491018a4fdf564b6491
matgar94 <mateo.gargicevich@gmail.com>
2023-05-16 09:52:45 -0300

    tiempo.js

--- js/tiempo.js
+++ js/tiempo.js
@@ -1,4 +1,14 @@
+  fetch('http://dataservice.accuweather.com/forecasts/v1/daily/1day/7894?apikey=DWJjhc7Kj0IxYGOnZOVT8jVmuSwV97G6&metric=true', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+    
+})
+.then(function (response) {
+    console.log(JSON.stringify(response))
+    return response.json();  
+  })
 
-fetch("http://dataservice.accuweather.com/forecasts/v1/daily/1day/7894?apikey=DWJjhc7Kj0IxYGOnZOVT8jVmuSwV97G6&metric=true")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+   .then(response => console.log(JSON.stringify(response)))
+   
